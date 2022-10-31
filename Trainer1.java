8af76fdaea132823490d208a9080633507fff90f
pspandana <spandana.pullela@gmail.com>
2022-10-31 08:43:28 -0500

    assignment 2 files

---
+++ Trainer1.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+
+
+public class Trainer1 {
+	public static void main(String[] args) {
+		String[] a= {"Name: Mukesh", "Department : Testing", "Gmail: mukesh@gmail.com", "ID:12890"};
+		for(String x:a) {
+			
+			System.out.println(x);
+		}
+		
+	}
+	
+	
+}
